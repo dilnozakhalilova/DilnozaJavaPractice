c45032a512b67f5ab67431e645a91101b9ac8fc4
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-09-27 23:21:12 -0400

    commit

--- src/Task.java
+++ src/Task.java
@@ -2,5 +2,6 @@ public class Task {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Practice");
+        System.out.println("Hello");
     }
 }
